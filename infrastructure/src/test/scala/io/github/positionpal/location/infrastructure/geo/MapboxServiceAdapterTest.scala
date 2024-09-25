0d5864ef023fdb1dfd64c75a8f617642459c05c5
Luca Tassinari <luca.tassinari.2000@gmail.com>
2024-09-25 12:08:56 +0200

    test(infrastructure): more flexible test

--- infrastructure/src/test/scala/io/github/positionpal/location/infrastructure/geo/MapboxServiceAdapterTest.scala
+++ infrastructure/src/test/scala/io/github/positionpal/location/infrastructure/geo/MapboxServiceAdapterTest.scala
@@ -8,18 +8,19 @@ import io.github.positionpal.location.commons.EnvVariablesProvider
 import io.github.positionpal.location.domain.RoutingMode
 import io.github.positionpal.location.domain.RoutingMode.Driving
 import io.github.positionpal.location.infrastructure.utils.*
-import io.github.positionpal.location.infrastructure.utils.MapboxServiceAdapterUtils.*
 import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.matchers.should.Matchers
 
 class MapboxServiceAdapterTest extends AnyFunSpec with Matchers:
 
+  private val mapboxService = MapboxService.Adapter()
+
   describe("The Mapbox service adapter"):
     it("should calculate the distance between two locations"):
       val distanceRequest = for
         envs <- EnvVariablesProvider[IO].configuration
-        config <- clientResource.use(client => IO.pure(MapboxService.Configuration(client, envs("MAPBOX_API_KEY"))))
-        distance <- mapboxServiceAdapter.distance(Driving)(cesenaCampus, bolognaCampus).value.run(config)
+        config <- HTTPUtils.client.use(client => IO.pure(MapboxService.Configuration(client, envs("MAPBOX_API_KEY"))))
+        distance <- mapboxService.distance(Driving)(cesenaCampus, bolognaCampus).value.run(config)
       yield distance
       val result = distanceRequest.unsafeRunSync()
       result.isRight shouldBe true
@@ -29,8 +30,8 @@ class MapboxServiceAdapterTest extends AnyFunSpec with Matchers:
     it("should calculate the arrival time between two locations"):
       val arrivalTimeRequest = for
         envs <- EnvVariablesProvider[IO].configuration
-        config <- clientResource.use(client => IO.pure(MapboxService.Configuration(client, envs("MAPBOX_API_KEY"))))
-        arrivalTime <- mapboxServiceAdapter.duration(Driving)(cesenaCampus, bolognaCampus).value.run(config)
+        config <- HTTPUtils.client.use(client => IO.pure(MapboxService.Configuration(client, envs("MAPBOX_API_KEY"))))
+        arrivalTime <- mapboxService.duration(Driving)(cesenaCampus, bolognaCampus).value.run(config)
       yield arrivalTime
       val result = arrivalTimeRequest.unsafeRunSync()
       result.isRight shouldBe true
