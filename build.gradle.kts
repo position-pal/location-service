13bea85ad175dbf042b2514f5512ed85853b68d0
Luca Tassinari <luca.tassinari.2000@gmail.com>
2024-10-01 22:00:15 +0200

    refactor: move to DotenvUtils

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import DotenvUtils.dotenv
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 
@@ -6,24 +7,6 @@ plugins {
     id("scala")
     alias(libs.plugins.scala.extras)
 }
-
-class DotenvConfiguration(private val fileName: String = DEFAULT_ENV_FILE_NAME) {
-
-    fun environmentVariables(): Map<String, String> =
-        rootDir.resolve(fileName)
-            .takeIf { it.exists() }
-            ?.readLines()
-            ?.filter { it.isNotBlank() && !it.startsWith(COMMENT_SYMBOL) }
-            ?.associate { it.split(KEY_VALUE_SEPARATOR).let { (key, value) -> key to value } }
-            ?: emptyMap()
-
-    companion object {
-        private const val DEFAULT_ENV_FILE_NAME = ".env"
-        private const val COMMENT_SYMBOL = "#"
-        private const val KEY_VALUE_SEPARATOR = "="
-    }
-}
-val dotenvConfig = DotenvConfiguration()
 
 allprojects {
     group = "io.github.positionpal"
@@ -60,10 +43,10 @@ allprojects {
     }
 
     tasks.withType<JavaExec> {
-        dotenvConfig.environmentVariables().forEach { environment(it.key, it.value) }
+        rootProject.dotenv().environmentVariables().forEach { environment(it.key, it.value) }
     }
 
     tasks.withType<Test> {
-        dotenvConfig.environmentVariables().forEach { environment(it.key, it.value) }
+        rootProject.dotenv().environmentVariables().forEach { environment(it.key, it.value) }
     }
 }
--- buildSrc/src/main/kotlin/DotenvUtils.kt
+++ buildSrc/src/main/kotlin/DotenvUtils.kt
@@ -1,7 +1,10 @@
 import org.gradle.api.Project
+import org.gradle.process.ProcessForkOptions
 import java.io.File
 
-object Utils {
+object DotenvUtils {
+
+    fun Project.dotenv(): DotenvConfiguration = DotenvConfiguration(projectDir)
 
     class DotenvConfiguration(private val rootDir: File, private val fileName: String = DEFAULT_ENV_FILE_NAME) {
 
@@ -13,13 +16,13 @@ object Utils {
                 ?.associate { it.split(KEY_VALUE_SEPARATOR).let { (key, value) -> key to value } }
                 ?: emptyMap()
 
+        fun applyTo(vararg pfo: ProcessForkOptions) =
+            environmentVariables().forEach { (key, value) -> pfo.forEach { it.environment(key, value) } }
+
         companion object {
             private const val DEFAULT_ENV_FILE_NAME = ".env"
             private const val COMMENT_SYMBOL = "#"
             private const val KEY_VALUE_SEPARATOR = "="
         }
     }
-
-    fun Project.envs() = DotenvConfiguration(projectDir).environmentVariables()
-
 }
