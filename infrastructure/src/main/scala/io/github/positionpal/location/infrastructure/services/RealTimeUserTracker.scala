45521d0d3dcfba25ebd51db1a3f37bdfb57c17fc
Luca Tassinari <luca.tassinari.2000@gmail.com>
2024-10-13 15:34:58 +0200

    refactor(infrastructure): leverage implicits for context

--- infrastructure/src/main/scala/io/github/positionpal/location/infrastructure/services/RealTimeUserTracker.scala
+++ infrastructure/src/main/scala/io/github/positionpal/location/infrastructure/services/RealTimeUserTracker.scala
@@ -21,7 +21,7 @@ import io.github.positionpal.location.infrastructure.geo.*
 object RealTimeUserTracker:
 
   /** Uniquely identifies the types of this entity instances (actors) that will be managed by cluster sharding. */
-  val typeKey: EntityTypeKey[Command] = EntityTypeKey[Command](getClass.getName)
+  val key: EntityTypeKey[Command] = EntityTypeKey[Command](getClass.getName)
 
   case object Ignore
   case object AliveCheck
@@ -32,30 +32,21 @@ object RealTimeUserTracker:
 
   final case class State(
       userState: UserState,
-      tracking: Option[T], // TODO: keep in memory a subset of the tracking data!
+      tracking: Option[T],
       lastSample: Option[SampledLocation],
   ) extends Serializable
   object State:
     def empty: State = State(UserState.Inactive, None, None)
 
-  /** Configure this actor to be managed by cluster sharding.
-    * @return the [[Entity]] instance that will be managed by cluster sharding.
-    */
-  def apply(): Entity[Command, ShardingEnvelope[Command]] =
-    Entity(typeKey): entityCtx =>
-      apply(entityCtx.entityId)
+  def apply(): Entity[Command, ShardingEnvelope[Command]] = Entity(key): entityCtx =>
+    this(entityCtx.entityId)
 
-  def apply(entityId: String): Behavior[Command] =
-    Behaviors.setup: ctx =>
-      Behaviors.withTimers: timer =>
-        ctx.log.debug("Starting RealTimeUserTracker::{}@{}", entityId, Cluster(ctx.system).selfMember.address)
-        timer.startTimerAtFixedRate(AliveCheck, 20.seconds)
-        EventSourcedBehavior(
-          PersistenceId(typeKey.name, entityId),
-          State.empty,
-          commandHandler(ctx, timer),
-          eventHandler,
-        )
+  def apply(entityId: String): Behavior[Command] = Behaviors.setup: ctx =>
+    given ActorContext[Command] = ctx
+    Behaviors.withTimers: timer =>
+      ctx.log.debug("Starting RealTimeUserTracker::{}@{}", entityId, Cluster(ctx.system).selfMember.address)
+      timer.startTimerAtFixedRate(AliveCheck, 20.seconds)
+      EventSourcedBehavior(PersistenceId(key.name, entityId), State.empty, commandHandler(timer), eventHandler)
 
   private val eventHandler: (State, Event) => State = (state, event) =>
     event match
@@ -69,26 +60,24 @@ object RealTimeUserTracker:
       case _: WentOffline => State(Inactive, state.tracking, state.lastSample)
 
   private def commandHandler(
-      ctx: ActorContext[Command],
       timer: TimerScheduler[Command],
-  ): (State, Command) => Effect[Event, State] = (state, command) =>
+  )(using ActorContext[Command]): (State, Command) => Effect[Event, State] = (state, command) =>
     command match
-      case ev: SampledLocation => trackingHandler(ctx)(state, ev)
+      case ev: SampledLocation => trackingHandler(state, ev)
       case ev: (RoutingStarted | RoutingStopped | SOSAlertTriggered | SOSAlertStopped) => Effect.persist(ev)
       case ev: AliveCheck.type => aliveCheckHandler(timer)(state, ev)
       case _ => Effect.none
 
-  private def trackingHandler(ctx: ActorContext[Command]): (State, SampledLocation) => Effect[Event, State] =
+  private def trackingHandler(using ctx: ActorContext[Command]): (State, SampledLocation) => Effect[Event, State] =
     import cats.effect.unsafe.implicits.global
     (state, event) =>
       state match
         case State(Routing, Some(tracking: MonitorableTracking), _) =>
           ctx.pipeToSelf(reaction(tracking, event).unsafeToFuture()):
-            case Success(result) => reactionHandler(ctx)(event)(result)
+            case Success(result) => reactionHandler(event)(result)
             case Failure(exception) =>
               ctx.log.error(exception.getMessage)
               Ignore
-          ctx.log.debug("Routing...")
           Effect.persist(event)
         case _ => Effect.persist(event)
 
@@ -99,12 +88,10 @@ object RealTimeUserTracker:
       result <- checks(tracking, event).value.run(config)
     yield result.flatten
 
-  private def reactionHandler(ctx: ActorContext[Command])(event: Event)(
-      result: Either[java.io.Serializable, TrackingEventReaction.Continue.type],
+  private def reactionHandler(event: Event)(result: Either[java.io.Serializable, TrackingEventReaction.Continue.type])(
+      using ctx: ActorContext[Command],
   ): Command = result match
-    case Right(_) =>
-      ctx.log.debug("Routing continuing...")
-      Ignore
+    case Right(_) => Ignore
     case Left(Notification.Alert(msg)) =>
       ctx.log.debug(msg)
       Ignore
