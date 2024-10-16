7970f2b9851ba1241d17df5f5f331477f95529ac
Luca Tassinari <luca.tassinari.2000@gmail.com>
2024-10-16 09:45:27 +0200

    refactor(commons): rename file and add docs

--- commons/src/main/scala/io/github/positionpal/location/commons/ScopeFunctions.scala
+++ commons/src/main/scala/io/github/positionpal/location/commons/ScopeFunctions.scala
@@ -1,5 +1,10 @@
 package io.github.positionpal.location.commons
 
+/** A set of scope functions (inspired by Kotlin) to improve the readability of the code. */
 object ScopeFunctions:
 
-  extension [A](a: A) def also(f: A => Unit): A = { f(a); a }
+  extension [A](a: A)
+    /** @return `this` after having applied the given function [[f]] to it. */
+    def also(f: A => Unit): A =
+      f(a)
+      a
