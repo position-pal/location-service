7ca5e91ce110354fdce93367f8c899104134a177
Luca Tassinari <luca.tassinari.2000@gmail.com>
2024-10-17 13:25:13 +0200

    refactor: remove useless types and constructor returns directly a resource

--- infrastructure/src/main/scala/io/github/positionpal/location/infrastructure/services/AkkaSerializable.scala
+++ infrastructure/src/main/scala/io/github/positionpal/location/infrastructure/services/AkkaSerializable.scala
@@ -1,7 +1,12 @@
 package io.github.positionpal.location.infrastructure.services
 
+import akka.actor.typed.ActorSystem
+
 /** Marker trait for remote serializable messages exchanged between distributed actors.
   * Every message class exchanged between distributed actors should extend this trait
   * (which is referenced as binding in the Akka configuration).
   */
 trait AkkaSerializable
+
+trait AkkaActorSystemProvider[-T]:
+  def system: ActorSystem[T]
--- infrastructure/src/main/scala/io/github/positionpal/location/infrastructure/services/RealTimeTrackingService.scala
+++ infrastructure/src/main/scala/io/github/positionpal/location/infrastructure/services/RealTimeTrackingService.scala
@@ -5,36 +5,28 @@ import akka.actor.typed.scaladsl.Behaviors
 import akka.cluster.sharding.typed.scaladsl.ClusterSharding
 import cats.effect.kernel.Resource.eval
 import cats.effect.{Async, Resource}
-import cats.implicits.{toFlatMapOps, toFunctorOps}
-import cats.mtl.{Ask, Stateful}
+import cats.mtl.Ask
 import com.typesafe.config.Config
-import io.github.positionpal.location.application.services.{RealTimeTrackingService, Startable}
-import io.github.positionpal.location.commons.{CanAsk, HasState}
+import io.github.positionpal.location.application.services.RealTimeTrackingService
+import io.github.positionpal.location.commons.CanAsk
 import io.github.positionpal.location.domain.DomainEvent
 import io.github.positionpal.location.infrastructure.utils.AkkaUtils
 
 object RealTimeTrackingService:
 
   /** Creates an Akka Actor based [[RealTimeTrackingService]]. */
-  def apply[F[_]: Async: CanAsk[Config]: HasState[ActorSystem[DomainEvent]]]: RealTimeTrackingService[F] =
-    ActorBasedRealTimeTrackingService()
+  def apply[F[_]: Async: CanAsk[Config]]: Resource[F, RealTimeTrackingService[F] & AkkaActorSystemProvider[Any]] =
+    for
+      config <- eval(Ask[F, Config].ask)
+      system <- AkkaUtils.startup(config)(Behaviors.empty)
+      cluster <- eval(Async[F].delay(ClusterSharding(system)))
+      _ <- eval(Async[F].delay(cluster.init(RealTimeUserTracker())))
+    yield ActorBasedRealTimeTrackingService[F](system)
 
-  private class ActorBasedRealTimeTrackingService[F[_]: Async: CanAsk[Config]: HasState[ActorSystem[DomainEvent]]]
+  private class ActorBasedRealTimeTrackingService[F[_]: Async](override val system: ActorSystem[Any])
       extends RealTimeTrackingService[F]
-      with Startable[[A] =>> Resource[F, A], ActorSystem[DomainEvent]]:
-
-    override def start: Resource[F, ActorSystem[DomainEvent]] =
-      for
-        config <- eval(Ask[F, Config].ask)
-        system <- AkkaUtils.startup(config)(Behaviors.empty)
-        cluster <- eval(Async[F].delay(ClusterSharding(system)))
-        _ <- eval(Stateful[F, ActorSystem[DomainEvent]].modify(_ => system))
-        _ <- eval(Async[F].delay(cluster.init(RealTimeUserTracker())))
-      yield system
+      with AkkaActorSystemProvider[Any]:
 
     override def handle(event: DomainEvent): F[Unit] =
-      for
-        actorSystem <- Stateful[F, ActorSystem[DomainEvent]].get
-        _ <- Async[F].delay:
-          ClusterSharding(actorSystem).entityRefFor(RealTimeUserTracker.key, event.user.id) ! event
-      yield ()
+      Async[F].delay:
+        ClusterSharding(system).entityRefFor(RealTimeUserTracker.key, event.user.id) ! event
