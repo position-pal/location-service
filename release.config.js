98217bcbdcbd29030729d0e545cd03c67fd35d0a
Luca Tassinari <luca.tassinari.2000@gmail.com>
2024-11-13 12:21:20 +0100

    fix: use correct repo name

--- release.config.js
+++ release.config.js
@@ -1,12 +1,11 @@
 const publishCommands = `
-DOCKER_IMAGE_NAME=\${process.env.GITHUB_REPOSITORY#*/}
-echo "Building image \${DOCKER_IMAGE_NAME}:\${nextRelease.version}..."
-docker build -t \${process.env.DOCKERHUB_USERNAME}/\${DOCKER_IMAGE_NAME}:\${nextRelease.version} -f ./Dockerfile-app . || exit 1
-docker tag \${process.env.DOCKERHUB_USERNAME}/\${DOCKER_IMAGE_NAME}:\${nextRelease.version} \${process.env.DOCKERHUB_USERNAME}/\${DOCKER_IMAGE_NAME}:latest || exit 2
+echo "Building image \${nextRelease.version}..."
+docker build -t \${process.env.DOCKERHUB_USERNAME}/location-service:\${nextRelease.version} -f ./Dockerfile-app . || exit 1
+docker tag \${process.env.DOCKERHUB_USERNAME}/location-service:\${nextRelease.version} \${process.env.DOCKERHUB_USERNAME}/location-service:latest || exit 2
 
 echo "Pushing Docker images..."
-docker push \${process.env.DOCKERHUB_USERNAME}/\${DOCKER_IMAGE_NAME}:\${nextRelease.version} || exit 3
-docker push \${process.env.DOCKERHUB_USERNAME}/\${DOCKER_IMAGE_NAME}:latest || exit 4
+docker push \${process.env.DOCKERHUB_USERNAME}/location-service:\${nextRelease.version} || exit 3
+docker push \${process.env.DOCKERHUB_USERNAME}/location-service:latest || exit 4
 
 echo "release_status=released" >> $GITHUB_ENV
 echo "CONTAINER_VERSION="\${nextRelease.version} >> $GITHUB_ENV
