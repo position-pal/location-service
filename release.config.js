6211b52d6971307fbe1dd675f0ab7648a8f10e76
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2024-11-14 13:33:10 +0100

    feat(ci): add docker release workflow

--- release.config.js
+++ release.config.js
@@ -1,18 +1,15 @@
+const config = require('semantic-release-preconfigured-conventional-commits')
+
 const publishCommands = `
-echo "Building image \${nextRelease.version}..."
-docker build -t \${process.env.DOCKERHUB_USERNAME}/location-service:\${nextRelease.version} -f ./Dockerfile-app . || exit 1
-docker tag \${process.env.DOCKERHUB_USERNAME}/location-service:\${nextRelease.version} \${process.env.DOCKERHUB_USERNAME}/location-service:latest || exit 2
-
-echo "Pushing Docker images..."
-docker push \${process.env.DOCKERHUB_USERNAME}/location-service:\${nextRelease.version} || exit 3
-docker push \${process.env.DOCKERHUB_USERNAME}/location-service:latest || exit 4
-
+git tag -a -f \${nextRelease.version} \${nextRelease.version} -F CHANGELOG.md || exit 2
+git push --force origin \${nextRelease.version} || exit 3
 echo "release_status=released" >> $GITHUB_ENV
 echo "CONTAINER_VERSION="\${nextRelease.version} >> $GITHUB_ENV
 `
 const releaseBranches = ["main"]
-const config = require('semantic-release-preconfigured-conventional-commits')
+
 config.branches = releaseBranches
+
 config.plugins.push(
     // Custom release commands
     ["@semantic-release/exec", {
@@ -21,4 +18,5 @@ config.plugins.push(
     "@semantic-release/github",
     "@semantic-release/git",
 )
+
 module.exports = config
