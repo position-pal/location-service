9f78afe5ec12c0f33cc26760826da795c8dd3aea
Luca Tassinari <luca.tassinari.2000@gmail.com>
2024-11-29 11:59:09 +0100

    feat(presentation): add grpc services definitions

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -1,8 +1,70 @@
+import com.google.protobuf.gradle.id
+import com.google.protobuf.gradle.remove
+import org.apache.tools.ant.taskdefs.condition.Os
+
+plugins {
+    alias(libs.plugins.protobuf)
+    alias(libs.plugins.scala.extras)
+}
+
 dependencies {
     api(project(":application"))
-    api(libs.bundles.circe)
-    api(libs.bundles.borer)
-    api(libs.positionpal.kernel.presentation)
-    api("org.apache.avro:avro:1.12.0")
-    implementation(libs.akka.actor)
+    with(libs) {
+        api(bundles.circe)
+        api(bundles.borer)
+        api(positionpal.kernel.presentation)
+        api(akka.actor)
+        api(fs2.grpc.runtime)
+        api(grpc.core)
+        api(grpc.stub)
+        api(grpc.protobuf)
+        api(protoc.gen.fs2)
+        api(scalapb.runtime)
+        api(scalapb.runtime.grpc)
+    }
+}
+
+protobuf {
+    protoc {
+        artifact = libs.protobuf.protoc.get().toString()
+    }
+    plugins {
+        val extension = if (Os.isFamily(Os.FAMILY_WINDOWS)) "${Os.FAMILY_WINDOWS}@bat" else "${Os.FAMILY_UNIX}@sh"
+        id("scalapb") {
+            artifact = "${libs.scalapb.protoc.gen.get()}:$extension"
+        }
+        id("fs2grpc") {
+            artifact = "${libs.protoc.gen.fs2.get()}:$extension"
+        }
+    }
+    generateProtoTasks {
+        all().forEach {
+            it.builtins {
+                remove("java")
+            }
+            it.plugins {
+                id("scalapb") {
+                    option("grpc")
+                    option("flat_package")
+                    option("scala3_sources")
+                }
+                id("fs2grpc") {
+                    option("flat_package")
+                }
+            }
+        }
+    }
+}
+
+sourceSets {
+    main {
+        scala {
+            srcDirs("${protobuf.generatedFilesBaseDir}/main/scalapb")
+            srcDirs("${protobuf.generatedFilesBaseDir}/main/fs2grpc")
+        }
+    }
+}
+
+scalafix {
+    excludes = setOf("**/proto/**")
 }
---
+++ presentation/src/main/scala/io/github/positionpal/location/presentation/ProtoConversions.scala
@@ -0,0 +1,53 @@
+package io.github.positionpal.location.presentation
+
+object ProtoConversions:
+  import io.github.positionpal.entities.{UserId, GroupId}
+  import io.github.positionpal.location.domain.*
+  import scalapb.TimestampConverters.fromJavaInstant
+
+  given uidToProto: Conversion[UserId, proto.UserId] = uid => proto.UserId(uid.username())
+
+  given protoToUid: Conversion[proto.UserId, UserId] = uid => UserId.create(uid.username)
+
+  given protoToGid: Conversion[GroupId, proto.GroupId] = gid => proto.GroupId(gid.value)
+
+  given gidToProto: Conversion[proto.GroupId, GroupId] = gid => GroupId.create(gid.value)
+
+  given stateToProto: Conversion[UserState, proto.UserState] = {
+    case UserState.Active => proto.UserState.ACTIVE
+    case UserState.Inactive => proto.UserState.INACTIVE
+    case UserState.SOS => proto.UserState.SOS
+    case UserState.Routing => proto.UserState.ROUTING
+  }
+
+  given locationToProto: Conversion[GPSLocation, proto.GPSLocation] = l => proto.GPSLocation(l.latitude, l.longitude)
+
+  given protoToLocation: Conversion[proto.GPSLocation, GPSLocation] = l => GPSLocation(l.latitude, l.longitude)
+
+  given sampledLocationToProto: Conversion[SampledLocation, proto.SampledLocation] = l =>
+    proto.SampledLocation(Some(l.user), Some(l.timestamp), Some(l.position))
+
+  given routeToProto: Conversion[Route, proto.Route] = r => proto.Route(r.map(identity))
+
+  given trackingToProto: Conversion[Tracking | MonitorableTracking, proto.Tracking] = {
+    case t: MonitorableTracking => proto.Tracking(Some(t.route), Some(t.destination), Some(t.expectedArrival))
+    case t: Tracking => proto.Tracking(Some(t.route))
+  }
+
+  given sessionToProto: Conversion[Session, proto.Session] = s => proto.Session(
+    Some(s.userId),
+    s.userState,
+    s.lastSampledLocation.map(sampledLocationToProto(_)),
+    s.tracking.map(trackingToProto(_))
+  )
+end ProtoConversions
+
+object ProtoUtils:
+  import io.github.positionpal.location.presentation.proto.Status
+
+  def OkResponse: Some[Status] = Some(proto.Status(proto.StatusCode.OK))
+
+  def NotFoundResponse(message: String): Some[Status] = Some(proto.Status(proto.StatusCode.NOT_FOUND, message))
+
+  def ErrorResponse(message: String): Some[Status] = Some(proto.Status(proto.StatusCode.GENERIC_ERROR, message))
+end ProtoUtils
