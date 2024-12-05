a3c399ff830c851b130f3b5c631511011102d96f
Luca Tassinari <luca.tassinari.2000@gmail.com>
2024-12-05 22:17:34 +0100

    feat(entrypoint): add entrypoint skeleton

--- entrypoint/build.gradle.kts
+++ entrypoint/build.gradle.kts
@@ -1,14 +1,50 @@
+import Utils.inCI
+import Utils.normally
+import Utils.onMac
+import Utils.onWindows
+
 plugins {
     application
+    alias(libs.plugins.cucumber.runner)
+    alias(libs.plugins.allure)
 }
 
 dependencies {
     implementation(project(":infrastructure"))
+    implementation(project(":ws"))
     implementation(project(":grpc"))
     implementation(project(":storage"))
     implementation(project(":messages"))
+    testImplementation(testFixtures(project(":domain")))
+    testImplementation(testFixtures(project(":ws")))
 }
 
 application {
-    mainClass.set("$group.location.entrypoint.main")
+    mainClass.set("$group.location.entrypoint.Launcher")
 }
+
+cucumber {
+    val featureFilesDirectory = "cucumber/features"
+    featurePath = "src/test/resources/$featureFilesDirectory"
+    glue = "$group.location"
+    extraGlues = arrayOf(
+//        "eu.delimata.stepdefinitions",
+    )
+    main = "io.cucumber.core.cli.Main"
+    plugin = arrayOf(
+        "pretty",
+//        "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
+    )
+}
+
+tasks.test {
+    finalizedBy("cucumber")
+}
+
+normally {
+    dockerCompose {
+        isRequiredBy(tasks.cucumber)
+    }
+} except { inCI and (onMac or onWindows) } where {
+    tasks.test { enabled = false }
+} cause "GitHub Actions runner does not support Docker Compose"
