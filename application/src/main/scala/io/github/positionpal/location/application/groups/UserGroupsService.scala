ed680b79deba95e5e98cc0850ec98e1990c85b13
Luca Tassinari <luca.tassinari.2000@gmail.com>
2024-12-05 22:14:02 +0100

    refactor(application): drop `Async` bound to effect type

--- application/src/main/scala/io/github/positionpal/location/application/groups/UserGroupsService.scala
+++ application/src/main/scala/io/github/positionpal/location/application/groups/UserGroupsService.scala
@@ -1,13 +1,12 @@
 package io.github.positionpal.location.application.groups
 
-import cats.effect.kernel.Async
 import io.github.positionpal.entities.{GroupId, UserId}
 import io.github.positionpal.{AddedMemberToGroup, RemovedMemberToGroup}
 
 /** A service for managing user groups.
   * @tparam F the effect type
   */
-trait UserGroupsService[F[_]: Async]:
+trait UserGroupsService[F[_]]:
 
   /** Add a member to a group described by the given [[event]]. */
   def addedMember(event: AddedMemberToGroup): F[Unit]
