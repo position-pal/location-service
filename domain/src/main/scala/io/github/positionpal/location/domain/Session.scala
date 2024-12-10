4e993f518407efd73282458d37c550bbd49d1f55
Luca Tassinari <luca.tassinari.2000@gmail.com>
2024-12-10 09:45:29 +0100

    refactor(domain): make session scoped to a specific user and group

--- domain/src/main/scala/io/github/positionpal/location/domain/Session.scala
+++ domain/src/main/scala/io/github/positionpal/location/domain/Session.scala
@@ -1,12 +1,20 @@
 package io.github.positionpal.location.domain
 
-import io.github.positionpal.entities.UserId
+import io.github.positionpal.entities.{GroupId, UserId}
+
+/** Defines the context in which a user's state is visible and relevant to a specific group.
+  * A `Scope` captures the idea that a user's state can differ depending on the group
+  * they are associated with, enabling group-specific visibility and tracking.
+  * @param user  The unique identifier of the user.
+  * @param group The unique identifier of the group.
+  */
+case class Scope(user: UserId, group: GroupId)
 
 /** A facade over the user's state and tracking information. */
 trait Session:
 
-  /** The identifier of the user. */
-  def userId: UserId
+  /** The [[Scope]] of this session. */
+  def scope: Scope
 
   /** The current state of the user. */
   def userState: UserState
@@ -27,29 +35,48 @@ object Session:
   import io.github.positionpal.location.domain.EventConversions.given
 
   /** A snapshot of the user's state and tracking information. */
-  final case class Snapshot(userId: UserId, userState: UserState, lastSampledLocation: Option[SampledLocation])
+  final case class Snapshot(scope: Scope, userState: UserState, lastSampledLocation: Option[SampledLocation])
 
   extension (s: Session)
     /** @return a [[Snapshot]] from the current [[Session]]. */
-    def toSnapshot: Snapshot = Snapshot(s.userId, s.userState, s.lastSampledLocation)
+    def toSnapshot: Snapshot = Snapshot(s.scope, s.userState, s.lastSampledLocation)
 
-  def unapply(
-      s: Session,
-  ): Option[(UserId, UserState, Option[SampledLocation], Option[Tracking | MonitorableTracking])] =
-    Some((s.userId, s.userState, s.lastSampledLocation, s.tracking))
-
-  /** Creates a new [[Session]] for the given [[userId]],
-    * initially in the [[Inactive]] state with no tracking information.
+  /** Extractor method for a [[Session]], allowing pattern matching to deconstruct it into its components.
+    * @param s The [[Session]] instance to deconstruct.
+    * @return an [[Option]]al tuple with the [[Scope]], [[UserState]], [[SampledLocation]] and [[Tracking]] information.
     */
-  def of(userId: UserId): Session =
-    SessionImpl(userId, userState = Inactive, lastSampledLocation = None, tracking = None)
+  def unapply(s: Session): Option[(Scope, UserState, Option[SampledLocation], Option[Tracking | MonitorableTracking])] =
+    Some((s.scope, s.userState, s.lastSampledLocation, s.tracking))
 
-  /** Creates a new [[Session]] from the given [[userId]], [[state]], [[lastSample]] and [[tracking]] information. */
-  def from(userId: UserId, state: UserState, lastSample: Option[SampledLocation], tracking: Option[Tracking]): Session =
-    SessionImpl(userId, state, lastSample, tracking)
+  /** Creates a new [[Session]] for the given [[groupId]] - [[userId]] initially in the [[Inactive]] state
+    * with no tracking information.
+    * @param groupId The unique identifier of the group.
+    * @param userId The unique identifier of the user.
+    */
+  def of(groupId: GroupId, userId: UserId): Session = of(Scope(userId, groupId))
+
+  /** Creates a new [[Session]] for the given [[Scope]] initially in the [[Inactive]] state
+    * with no tracking information.
+    */
+  def of(scope: Scope): Session = SessionImpl(scope, userState = Inactive, lastSampledLocation = None, tracking = None)
+
+  /** Creates a new [[Session]] from the given [[groupId]], [[userId]], [[state]],
+    * [[lastSample]] and [[tracking]] information.
+    */
+  def from(
+      groupId: GroupId,
+      userId: UserId,
+      state: UserState,
+      lastSample: Option[SampledLocation],
+      tracking: Option[Tracking],
+  ): Session = from(Scope(userId, groupId), state, lastSample, tracking)
+
+  /** Creates a new [[Session]] from the given [[scope]], [[state]], [[lastSample]] and [[tracking]] information. */
+  def from(scope: Scope, state: UserState, lastSample: Option[SampledLocation], tracking: Option[Tracking]): Session =
+    SessionImpl(scope, state, lastSample, tracking)
 
   private case class SessionImpl(
-      override val userId: UserId,
+      override val scope: Scope,
       override val userState: UserState,
       override val lastSampledLocation: Option[SampledLocation],
       override val tracking: Option[Tracking | MonitorableTracking],
