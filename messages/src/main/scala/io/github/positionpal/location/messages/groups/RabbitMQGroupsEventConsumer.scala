94e5c1294b73a265debb7256e7719a1ae1580a30
Luca Tassinari <luca.tassinari.2000@gmail.com>
2024-12-23 10:27:44 +0100

    refactor(entrypoint): improve readability of entrypoint

--- messages/src/main/scala/io/github/positionpal/location/messages/groups/RabbitMQGroupsEventConsumer.scala
+++ messages/src/main/scala/io/github/positionpal/location/messages/groups/RabbitMQGroupsEventConsumer.scala
@@ -8,32 +8,36 @@ import io.github.positionpal.{AvroSerializer, MessageType}
 import lepus.client.{Connection, ConsumeMode, Message}
 import lepus.protocol.domains.{ExchangeType, FieldTable, ShortString}
 
-/** A consumer of groups-related events from RabbitMQ broker.
-  * @param userGroupsService the service to handle the events
-  * @tparam F the effect type
-  */
-class RabbitMQGroupsEventConsumer[F[_]: Async](userGroupsService: UserGroupsService[F]) extends RabbitMQ.Utils:
+/** A consumer of groups-related events from RabbitMQ broker. */
+object RabbitMQGroupsEventConsumer:
 
-  private val serializer = AvroSerializer()
+  import cats.implicits.catsSyntaxApplicativeId
 
-  def start(connection: Connection[F]): F[Unit] = connection.channel.use: ch =>
-    for
-      _ <- ch.exchange.declare(groupsEventsExchange, ExchangeType.Headers)
-      q <- ch.queue.declare(groupsEventsQueue, autoDelete = false, durable = true, exclusive = false)
-      q <- Async[F].fromOption(q, QueueDeclarationFailed)
-      _ <- ch.queue.bind(q.queue, groupsEventsExchange, ShortString.empty)
-      consumer = ch.messaging.consume[Array[Byte]](q.queue, mode = ConsumeMode.RaiseOnError(true))
-        .evalMap(e => handleGroupEvent(e.message))
-      _ <- consumer.compile.drain
-    yield ()
+  def apply[F[_]: Async](userGroupsService: UserGroupsService[F]): F[RabbitMQGroupsEventConsumerImpl[F]] =
+    RabbitMQGroupsEventConsumerImpl(userGroupsService).pure[F]
 
-  private def handleGroupEvent(event: Message[Array[Byte]]): F[Unit] = event.properties.headers match
-    case Some(headers) if Header.memberAdded in headers =>
-      userGroupsService.addedMember(serializer.deserializeAddedMemberToGroup(event.payload))
-    case Some(headers) if Header.memberRemoved in headers =>
-      userGroupsService.removeMember(serializer.deserializeRemovedMemberToGroup(event.payload))
-    case _ => Async[F].unit
+  class RabbitMQGroupsEventConsumerImpl[F[_]: Async](userGroupsService: UserGroupsService[F]) extends RabbitMQ.Utils:
 
-  private object Header:
-    val memberAdded: ShortString = MessageType.MEMBER_ADDED.name().asShortOrEmpty
-    val memberRemoved: ShortString = MessageType.MEMBER_REMOVED.name().asShortOrEmpty
+    private val serializer = AvroSerializer()
+
+    def start(connection: Connection[F]): F[Unit] = connection.channel.use: ch =>
+      for
+        _ <- ch.exchange.declare(groupsEventsExchange, ExchangeType.Headers)
+        q <- ch.queue.declare(groupsEventsQueue, autoDelete = false, durable = true, exclusive = false)
+        q <- Async[F].fromOption(q, QueueDeclarationFailed)
+        _ <- ch.queue.bind(q.queue, groupsEventsExchange, ShortString.empty)
+        consumer = ch.messaging.consume[Array[Byte]](q.queue, mode = ConsumeMode.RaiseOnError(true))
+          .evalMap(e => handleGroupEvent(e.message))
+        _ <- consumer.compile.drain
+      yield ()
+
+    private def handleGroupEvent(event: Message[Array[Byte]]): F[Unit] = event.properties.headers match
+      case Some(headers) if Header.memberAdded in headers =>
+        userGroupsService.addedMember(serializer.deserializeAddedMemberToGroup(event.payload))
+      case Some(headers) if Header.memberRemoved in headers =>
+        userGroupsService.removeMember(serializer.deserializeRemovedMemberToGroup(event.payload))
+      case _ => Async[F].unit
+
+    private object Header:
+      val memberAdded: ShortString = MessageType.MEMBER_ADDED.name().asShortOrEmpty
+      val memberRemoved: ShortString = MessageType.MEMBER_REMOVED.name().asShortOrEmpty
