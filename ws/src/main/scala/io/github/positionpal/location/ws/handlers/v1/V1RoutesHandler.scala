7841ccfcdc661c047f6087b66909947204d13b76
Luca Tassinari <luca.tassinari.2000@gmail.com>
2024-12-25 17:51:07 +0100

    refactor(ws): log income and outgoing messages

--- ws/src/main/scala/io/github/positionpal/location/ws/handlers/v1/V1RoutesHandler.scala
+++ ws/src/main/scala/io/github/positionpal/location/ws/handlers/v1/V1RoutesHandler.scala
@@ -4,11 +4,13 @@ import scala.concurrent.ExecutionContext
 
 import akka.NotUsed
 import akka.actor.typed.{ActorRef, ActorSystem}
+import akka.event.Logging
+import akka.event.Logging.*
 import akka.http.scaladsl.model.ws.{Message, TextMessage}
 import akka.http.scaladsl.server.RouteResult.Complete
-import akka.stream.OverflowStrategy
 import akka.stream.scaladsl.{Flow, Sink, Source}
 import akka.stream.typed.scaladsl.ActorSource
+import akka.stream.{Attributes, OverflowStrategy}
 import cats.effect.IO
 import io.bullet.borer.Json
 import io.github.positionpal.entities.{GroupId, UserId}
@@ -27,27 +29,34 @@ class V1RoutesHandler(service: ActorBasedRealTimeTracking.Service[IO, Scope])(us
 
   override def handleTrackingRoute(userId: UserId, groupId: GroupId): Flow[Message, Message, NotUsed] =
     val scope = Scope(userId, groupId)
-    val routeToGroupActor: Sink[Message, Unit] = Flow[Message].map:
+    Flow.fromSinkAndSource(routeToGroupActor(scope), routeToClient(scope))
+
+  private def routeToGroupActor(scope: Scope): Sink[Message, Unit] =
+    Flow[Message].map:
       case TextMessage.Strict(msg) => Json.decode(msg.getBytes).to[DrivingEvent].valueEither
-      case _ => Left("Invalid message")
-    .collect { case Right(e) => e }.watchTermination(): (_, done) =>
-      done.onComplete: _ =>
-        val oldConnections = ConnectionsManager.removeConnection(groupId, userId)
-        val scopeActorRef = oldConnections.find(_._1 == userId).map(_._2)
-        scopeActorRef.foreach: ref =>
-          oldConnections.foreach((uid, _) => service.removeObserverFor(Scope(uid, groupId))(Set(ref)).unsafeRunSync())
-    .to(Sink.foreach(service.handle(scope)(_).unsafeRunSync()))
-    val routeToClient: Source[Message, ActorRef[DrivenEvent]] =
-      ActorSource.actorRef(
-        completionMatcher = { case Complete => },
-        failureMatcher = { case ex: Throwable => ex },
-        bufferSize = 1_000,
-        overflowStrategy = OverflowStrategy.fail,
-      ).mapMaterializedValue: ref =>
-        val oldConnections = ConnectionsManager.addConnection(groupId, userId, ref)
-        oldConnections.foreach((uid, _) => service.addObserverFor(Scope(uid, groupId))(Set(ref)).unsafeRunSync())
-        service.addObserverFor(scope)(oldConnections.map(_._2) + ref).unsafeRunSync()
-        ref
-      .map:
-        case event: DrivenEvent => TextMessage(Json.encode(event).toUtf8String)
-    Flow.fromSinkAndSource(routeToGroupActor, routeToClient)
+      case t => Left(s"Unsupported message type $t")
+    .log("Websocket message from client to tracker").withAttributes(attributes).collect { case Right(e) => e }
+      .watchTermination(): (_, done) =>
+        done.onComplete: _ =>
+          val oldConnections = ConnectionsManager.removeConnection(scope.group, scope.user)
+          val scopeActorRef = oldConnections.find(_._1 == scope.user).map(_._2)
+          scopeActorRef.foreach: ref =>
+            oldConnections
+              .foreach((uid, _) => service.removeObserverFor(Scope(uid, scope.group))(Set(ref)).unsafeRunSync())
+      .to(Sink.foreach(service.handle(scope)(_).unsafeRunSync()))
+
+  private def routeToClient(scope: Scope): Source[Message, ActorRef[DrivenEvent]] =
+    ActorSource.actorRef(
+      completionMatcher = { case Complete => },
+      failureMatcher = { case ex: Throwable => ex },
+      bufferSize = 1_000,
+      overflowStrategy = OverflowStrategy.fail,
+    ).mapMaterializedValue: ref =>
+      val oldConnections = ConnectionsManager.addConnection(scope.group, scope.user, ref)
+      oldConnections.foreach((uid, _) => service.addObserverFor(Scope(uid, scope.group))(Set(ref)).unsafeRunSync())
+      service.addObserverFor(scope)(oldConnections.map(_._2) + ref).unsafeRunSync()
+      ref
+    .log("Websocket message from tracker to client").withAttributes(attributes).map:
+      case event: DrivenEvent => TextMessage(Json.encode(event).toUtf8String)
+
+  private def attributes = Attributes.logLevels(onFinish = InfoLevel, onFailure = ErrorLevel, onElement = InfoLevel)
