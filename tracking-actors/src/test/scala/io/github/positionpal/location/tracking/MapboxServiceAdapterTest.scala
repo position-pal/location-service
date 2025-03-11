4601d49e3d13fb573ed12b5532d82546e3ae3429
Luca Tassinari <luca.tassinari.2000@gmail.com>
2025-03-11 08:58:11 +0100

    test(maps): use MAPBOX_TOKEN if present, otherwise mock the response

--- tracking-actors/src/test/scala/io/github/positionpal/location/tracking/MapboxServiceAdapterTest.scala
+++ tracking-actors/src/test/scala/io/github/positionpal/location/tracking/MapboxServiceAdapterTest.scala
@@ -2,22 +2,21 @@ package io.github.positionpal.location.tracking
 
 import io.github.positionpal.location.domain.Distance.DistanceUnit
 import io.github.positionpal.location.tracking.utils.HTTPUtils
+import cats.data.Kleisli
 import cats.effect.unsafe.implicits.global
+import org.http4s.*
+import org.http4s.client.Client
 import org.scalatest.matchers.should.Matchers
 import io.github.positionpal.location.domain.GeoUtils.*
 import org.scalatest.funspec.AnyFunSpec
 import io.github.positionpal.location.commons.EnvVariablesProvider
 import io.github.positionpal.location.domain.RoutingMode.Driving
-import cats.effect.IO
+import io.circe.parser.*
+import cats.effect.{IO, Resource}
 
 class MapboxServiceAdapterTest extends AnyFunSpec with Matchers:
 
-  private val mapServiceAdapter =
-    for
-      envs <- EnvVariablesProvider[IO].configuration
-      config <- HTTPUtils.clientRes.use(client => IO.pure(MapboxService.Configuration(client, envs("MAPBOX_API_KEY"))))
-      mapService <- MapboxService[IO](config)
-    yield mapService
+  import MapboxServiceAdapterTest.*
 
   describe("The Mapbox service adapter"):
     it("should calculate the distance between two locations"):
@@ -35,3 +34,45 @@ class MapboxServiceAdapterTest extends AnyFunSpec with Matchers:
         time <- mapService.duration(Driving)(cesenaCampus.position, bolognaCampus.position)
       yield time).unsafeRunSync()
       arrivalTime.toMinutes should be >= 60L // arrival time depends on the traffic!
+
+object MapboxServiceAdapterTest:
+
+  private val mapServiceAdapter =
+    for
+      envs <- EnvVariablesProvider[IO].configuration
+      tokenIsPreset = envs.contains("MAPBOX_API_KEY")
+      config <-
+        if tokenIsPreset then
+          HTTPUtils.clientRes.use(client => IO.pure(MapboxService.Configuration(client, envs("MAPBOX_API_KEY"))))
+        else HTTPUtils.clientRes.use(client => IO.pure(MapboxService.Configuration(mockedClient(client), "fake-token")))
+      mapService <- MapboxService[IO](config)
+    yield mapService
+
+  @SuppressWarnings(Array("scalafix:DisableSyntax.asInstanceOf"))
+  private def mockedClient(delegatingClient: Client[IO]): Client[IO] =
+    new Client[IO]:
+      export delegatingClient.{expect as _, run as _, *}
+      override def run(req: Request[IO]): Resource[IO, Response[IO]] = Resource.pure(Response[IO](Status.Ok))
+      override def expect[A](uri: Uri)(implicit d: EntityDecoder[IO, A]): IO[A] =
+        IO.pure(partialMockedMapboxResponse.asInstanceOf[A])
+      // The following methods are not used in the tests
+      override def expect[A](s: String)(implicit d: EntityDecoder[IO, A]): IO[A] = ???
+      override def expect[A](req: Request[IO])(implicit d: EntityDecoder[IO, A]): IO[A] = ???
+      override def expect[A](req: IO[Request[IO]])(implicit d: EntityDecoder[IO, A]): IO[A] = ???
+
+  /** A partial mocked Mapbox response containing only the routes information needed for the tests. */
+  private val partialMockedMapboxResponse = parse:
+    """
+      |{
+      |   "routes":[
+      |      {
+      |         "weight_name":"auto",
+      |         "weight":5706.868,
+      |         "duration":4934.365,
+      |         "distance":90393.367
+      |      }
+      |   ],
+      |   "code":"Ok"
+      |}
+      |""".stripMargin
+  .getOrElse(throw IllegalStateException("Malformed mocked mapbox response"))
